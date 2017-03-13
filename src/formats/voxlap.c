21101b959e1fc68d42a21f5e4c84bb5d667f836d
Guillaume Chereau <guillaume@noctua-software.com>
2017-03-13 16:22:48 +0800

    Add support for kvx vxlap files
    As with kv6, the axis are not correct yet.  Need to fix that...

--- src/formats/voxlap.c
+++ src/formats/voxlap.c
@@ -67,7 +67,6 @@ static int kv6_import(const char *path)
     READ(float, file);
     READ(float, file);
     blklen = READ(uint32_t, file);
-    LOG_D("blklen = %d", blklen);
     blocks = calloc(blklen, sizeof(*blocks));
     for (i = 0; i < blklen; i++) {
         blocks[i].color = READ(uint32_t, file);
@@ -120,6 +119,89 @@ end:
     return ret;
 }
 
+static int kvx_import(const char *path)
+{
+    FILE *file;
+    int i, r, ret = 0, nb, w, h, d, x, y, z, lastz = 0, len, visface;
+    uint8_t color = 0;
+    uvec4b_t *palette = NULL;
+    uint32_t *xoffsets = NULL;
+    uint16_t *xyoffsets = NULL;
+    uvec4b_t *cube = NULL;
+    long datpos;
+    (void)r;
+
+    path = path ?: noc_file_dialog_open(NOC_FILE_DIALOG_OPEN,
+                                        "kvx\0*.kvx\0", NULL, NULL);
+    if (!path) return -1;
+
+    file = fopen(path, "rb");
+    nb = READ(uint32_t, file); (void)nb;
+    w = READ(uint32_t, file);
+    h = READ(uint32_t, file);
+    d = READ(uint32_t, file);
+    cube = calloc(w * h * d, sizeof(*cube));
+
+    READ(uint32_t, file);
+    READ(uint32_t, file);
+    READ(uint32_t, file);
+
+    xoffsets = calloc(w + 1, sizeof(*xoffsets));
+    xyoffsets = calloc(w * (h + 1), sizeof(*xyoffsets));
+    for (i = 0; i < w + 1; i++)        xoffsets[i] = READ(uint32_t, file);
+    for (i = 0; i < w * (h + 1); i++) xyoffsets[i] = READ(uint16_t, file);
+
+    datpos = ftell(file);
+
+    // Read the palette at the end of the file first.
+    fseek(file, -256 * 3, SEEK_END);
+    palette = calloc(256, sizeof(*palette));
+    for (i = 0; i < 256; i++) {
+        palette[i].r = clamp(round(READ(uint8_t, file) * 255 / 63.f), 0, 255);
+        palette[i].g = clamp(round(READ(uint8_t, file) * 255 / 63.f), 0, 255);
+        palette[i].b = clamp(round(READ(uint8_t, file) * 255 / 63.f), 0, 255);
+        palette[i].a = 255;
+    }
+    fseek(file, datpos, SEEK_SET);
+
+    for (x = 0; x < w; x++)
+    for (y = 0; y < h; y++) {
+        if (xyoffsets[x * (h + 1) + y + 1] < xyoffsets[x * (h + 1) + y])
+            raise("Invalid format");
+        nb = xyoffsets[x * (h + 1) + y + 1] - xyoffsets[x * (h + 1) + y];
+        while (nb > 0) {
+            z = READ(uint8_t, file);
+            len = READ(uint8_t, file);
+            visface = READ(uint8_t, file);
+            assert(z + len - 1  < d);
+            for (i = 0; i < len; i++) {
+                color = READ(uint8_t, file);
+                cube[x + y * w + (z + i) * w * h] = palette[color];
+            }
+            nb -= len + 3;
+            // Fill
+            if (visface & 0x10) lastz = z + len;
+            if (visface & 0x20) {
+                for (i = lastz; i < z; i++)
+                    if (cube[x + y * w + i * w * h].a == 0)
+                        cube[x + y * w + i * w * h] = palette[color];
+            }
+        }
+    }
+
+    mesh_blit(goxel->image->active_layer->mesh, cube,
+              -w / 2, -h / 2, -d / 2, w, h, d);
+    goxel_update_meshes(goxel, -1);
+
+end:
+    free(palette);
+    free(cube);
+    free(xoffsets);
+    free(xyoffsets);
+    fclose(file);
+    return ret;
+}
+
 ACTION_REGISTER(import_kv6,
     .help = "Import a slab kv6 image",
     .cfunc = kv6_import,
@@ -127,5 +209,15 @@ ACTION_REGISTER(import_kv6,
     .file_format = {
         .name = "kv6",
         .ext = "*.kv6\0"
+    },
+)
+
+ACTION_REGISTER(import_kvx,
+    .help = "Import a slab kvx image",
+    .cfunc = kvx_import,
+    .csig = "vp",
+    .file_format = {
+        .name = "kvx",
+        .ext = "*.kvx\0"
     },
 )
