f0a9112be70842dd1df4bac0c4ae84e5f9f4b0f6
Guillaume Chereau <guillaume@noctua-software.com>
2017-03-30 12:01:54 +0800

    ios: detect enter key events

--- osx/goxel/goxel/AppDelegate.swift
+++ osx/goxel/goxel/AppDelegate.swift
@@ -79,6 +79,7 @@ class GoxNSOpenGLView: NSOpenGLView {
     
     override func keyDown(with event: NSEvent) {
         switch (event.keyCode) {
+            case 36: appDelegate().inputs.keys.257 = true
             case 51: appDelegate().inputs.keys.259 = true
             case 123: appDelegate().inputs.keys.263 = true
             case 124: appDelegate().inputs.keys.262 = true
@@ -95,6 +96,7 @@ class GoxNSOpenGLView: NSOpenGLView {
     
     override func keyUp(with event: NSEvent) {
         switch (event.keyCode) {
+            case 36: appDelegate().inputs.keys.257 = false
             case 51: appDelegate().inputs.keys.259 = false
             case 123: appDelegate().inputs.keys.263 = false
             case 124: appDelegate().inputs.keys.262 = false
