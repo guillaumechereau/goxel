238b0b735e6023a5aba648e85731257080dacd62
Guillaume Marie Bernard Chereau <guillaume@Guillaumes-MacBook-Air.local>
2017-04-19 14:52:35 +0800

    Add all xcode files
    I want to start to use xcode only to compile on mac, so that we don't
    have to install homebrew.

---
+++ osx/goxel/goxel/goxel-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  goxel-Bridging-Header.h
+//  goxel
+//
+//  Created by Guillaume Chereau on 10/16/15.
+//  Copyright © 2015 Noctua Software Limited. All rights reserved.
+//
+
+#ifndef goxel_Bridging_Header_h
+#define goxel_Bridging_Header_h
+
+#include "goxel.h"
+
+#endif /* goxel_Bridging_Header_h */
