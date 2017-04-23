252591772609a12af4ce76fd776d182d39f0dbac
Guillaume Chereau <guillaume@noctua-software.com>
2017-04-23 19:29:55 +0800

    Try to fix MODE_ADD operation
    It's a bit tricky, I should check how they do it in gimp maybe.

--- src/block.c
+++ src/block.c
@@ -405,15 +405,15 @@ static bool can_skip(uvec4b_t v, int mode, uvec4b_t c)
 static uvec4b_t combine(uvec4b_t a, uvec4b_t b, int mode)
 {
     uvec4b_t ret = a;
-    int i, aa = a.a, ba = b.a;
+    int aa = a.a, ba = b.a;
     if (mode == MODE_PAINT) {
         ret.rgb = uvec3b_mix(a.rgb, b.rgb, ba / 255.);
     }
     else if (mode == MODE_ADD) {
         ret.a = min((int)a.a + b.a, 255);
-        if (aa + ba)
-            for (i = 0; i < 3; i++)
-                ret.v[i] = (a.v[i] * aa + b.v[i] * ba) / (aa + ba);
+        if (aa + ba - (aa * ba / 255))
+            ret.rgb = uvec3b_mix(a.rgb, b.rgb,
+                                 ba / (aa + ba - aa * ba / 255.));
     }
     else if (mode == MODE_SUB) {
         ret.a = max(0, aa - ba);
