fd47a75130af9d7932fb281c2d4ff78dd01e4aa9
Guillaume Chereau <guillaume@noctua-software.com>
2017-08-01 17:30:42 +0800

    Minor change
    Still need to cleanup the popup code.

--- src/gui.cpp
+++ src/gui.cpp
@@ -983,6 +983,8 @@ static bool settings_popup(void)
     int i, nb, current;
     theme_t *themes = theme_get_list();
 
+    ImGui::BeginChild("body",
+            ImVec2(0, -ImGui::GetItemsLineHeightWithSpacing()));
     DL_COUNT(themes, theme, nb);
     names = (const char**)calloc(nb, sizeof(*names));
     i = 0;
@@ -1030,6 +1032,7 @@ static bool settings_popup(void)
     if (ImGui::Button("Save")) theme_save();
 #endif
 
+    ImGui::EndChild();
     return gui_button("OK", 0, 0);
 }
 
