2b189f80865183b6bb874b434166789ca4020779
Guillaume Chereau <guillaume@noctua-software.com>
2017-08-14 14:12:44 +0800

    Extrude tool first implementation
    Still not proper.  It fills the whole rect, instead of selecting the
    proper voxels.

--- src/tools/extrude.c
+++ src/tools/extrude.c
@@ -20,6 +20,9 @@
 
 typedef struct {
     tool_t tool;
+    mesh_t *mesh_orig;
+    mesh_t *mesh;
+    int    snap_face;
     struct {
         gesture3d_t drag;
     } gestures;
@@ -54,17 +57,57 @@ static int get_face(vec3_t n)
 
 static int on_drag(gesture3d_t *gest, void *user)
 {
+    tool_extrude_t *tool = (tool_extrude_t*)user;
     mesh_t *mesh = goxel->image->active_layer->mesh;
+    mesh_t *tmp_mesh;
     cursor_t *curs = gest->cursor;
+    box_t box;
+    plane_t face_plane;
+    vec3_t n, pos;
 
     if (gest->state == GESTURE_BEGIN) {
-        int snap_face = get_face(curs->normal);
-        mesh_t *test = mesh_new();
-        mesh_select(mesh, &curs->pos, select_cond, &snap_face, test);
-        mesh_merge(mesh, test, MODE_SUB);
-        mesh_delete(test);
+        tool->snap_face = get_face(curs->normal);
+        tool->mesh = mesh_new();
+        mesh_select(mesh, &curs->pos, select_cond, &tool->snap_face,
+                    tool->mesh);
+        mesh_set(tool->mesh_orig, mesh);
+        image_history_push(goxel->image);
+
+        // XXX: to remove: this is duplicated from selection tool.
+        box = mesh_get_box(tool->mesh, true);
+        face_plane.mat = mat4_mul(box.mat,
+                                  FACES_MATS[tool->snap_face]);
+        goxel->tool_plane = plane(curs->pos, curs->normal,
+                                  vec3_normalized(face_plane.u));
+    }
+
+    box = mesh_get_box(tool->mesh, true);
+
+    // XXX: have some generic way to resize boxes, since we use it all the
+    // time!
+    face_plane.mat = mat4_mul(box.mat, FACES_MATS[tool->snap_face]);
+    n = vec3_normalized(face_plane.n);
+    pos = vec3_add(goxel->tool_plane.p,
+                   vec3_project(vec3_sub(curs->pos, goxel->tool_plane.p), n));
+    pos.x = round(pos.x);
+    pos.y = round(pos.y);
+    pos.z = round(pos.z);
+    box = box_move_face(box, tool->snap_face, pos);
+    render_box(&goxel->rend, &box, NULL, EFFECT_WIREFRAME);
+
+    mesh_set(mesh, tool->mesh_orig);
+    tmp_mesh = mesh_copy(tool->mesh);
+    mesh_op(tmp_mesh, &goxel->painter, &box);
+    mesh_merge(mesh, tmp_mesh, MODE_OVER);
+    mesh_delete(tmp_mesh);
+    goxel_update_meshes(goxel, MESH_RENDER);
+
+    if (gest->state == GESTURE_END) {
+        mesh_delete(tool->mesh);
+        goxel->tool_plane = plane_null;
         goxel_update_meshes(goxel, -1);
     }
+
     return 0;
 }
 
@@ -74,6 +117,7 @@ static int iter(tool_t *tool_, const vec4_t *view)
     cursor_t *curs = &goxel->cursor;
     curs->snap_offset = -0.5;
 
+    if (!tool->mesh_orig) tool->mesh_orig = mesh_new();
     if (!tool->gestures.drag.type) {
         tool->gestures.drag = (gesture3d_t) {
             .type = GESTURE_DRAG,
