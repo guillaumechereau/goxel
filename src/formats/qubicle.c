4d14d12598d8209aa6190ae5afea8e953cd5b505
Guillaume Chereau <guillaume@noctua-software.com>
2018-01-26 00:03:01 +0800

    Try to improve qubicle export
    Thanks to Lynx3d for the suggestion.
    
    This saves the qubicle using right handed format, and uses one matrix
    per layer (instead of one per block).
    
    Probably some orientation bugs to fix.  I really need to add some kind
    of unit test for the import/exports functions!

--- src/formats/qubicle.c
+++ src/formats/qubicle.c
@@ -115,59 +115,54 @@ static void qubicle_import(const char *path)
     goxel_update_meshes(goxel, -1);
 }
 
-void qubicle_export(const mesh_t *mesh, const char *path)
+static void qubicle_export(const image_t *img, const char *path)
 {
     FILE *file;
-    int i, count, x, y, z, bpos[3], vpos[3];
+    int i, count, x, y, z, pos[3], bbox[2][3];
     uint8_t v[4];
     char buff[16];
-    mesh_t *m = mesh_copy(mesh);
-    mat4_t mat = mat4_identity;
+    layer_t *layer;
     mesh_iterator_t iter;
-
-    // Apply a -90 deg X rotation to fix axis.
-    mat4_irotate(&mat, -M_PI / 2, 1, 0, 0);
-    mesh_move(m, &mat);
-    mesh = m;
+    mesh_t *mesh;
 
     count = 0;
-    iter = mesh_get_iterator(mesh, MESH_ITER_BLOCKS);
-    while (mesh_iter(&iter, NULL)) count++;
+    DL_COUNT(img->layers, layer, count);
 
     file = fopen(path, "wb");
     WRITE(uint32_t, 257, file); // version
     WRITE(uint32_t, 0, file);   // color format RGBA
-    WRITE(uint32_t, 0, file);   // orientation left handed
+    WRITE(uint32_t, 1, file);   // orientation right handed
     WRITE(uint32_t, 0, file);   // no compression
     WRITE(uint32_t, 0, file);   // vmask
     WRITE(uint32_t, count, file);
 
     i = 0;
-    iter = mesh_get_iterator(mesh, MESH_ITER_BLOCKS);
-    while (mesh_iter(&iter, bpos)) {
+    DL_FOREACH(img->layers, layer) {
         sprintf(buff, "%d", i);
         WRITE(uint8_t, strlen(buff), file);
         fwrite(buff, strlen(buff), 1, file);
 
-        WRITE(uint32_t, BLOCK_SIZE, file);
-        WRITE(uint32_t, BLOCK_SIZE, file);
-        WRITE(uint32_t, BLOCK_SIZE, file);
-        WRITE(int32_t, bpos[0], file);
-        WRITE(int32_t, bpos[1], file);
-        WRITE(int32_t, bpos[2], file);
-        for (z = 0; z < BLOCK_SIZE; z++)
-        for (y = 0; y < BLOCK_SIZE; y++)
-        for (x = 0; x < BLOCK_SIZE; x++) {
-            vpos[0] = bpos[0] + x;
-            vpos[1] = bpos[1] + y;
-            vpos[2] = bpos[2] + z;
-            mesh_get_at(mesh, &iter, vpos, v);
+        mesh = layer->mesh;
+        mesh_get_bbox(mesh, bbox, true);
+        WRITE(uint32_t, bbox[1][0] - bbox[0][0], file);
+        WRITE(uint32_t, bbox[1][2] - bbox[0][2], file);
+        WRITE(uint32_t, bbox[1][1] - bbox[0][1], file);
+        WRITE(int32_t, bbox[0][0], file);
+        WRITE(int32_t, bbox[0][2], file);
+        WRITE(int32_t, bbox[0][1], file);
+        iter = mesh_get_accessor(mesh);
+        for (y = bbox[0][1]; y < bbox[1][1]; y++)
+        for (z = bbox[0][2]; z < bbox[1][2]; z++)
+        for (x = bbox[0][0]; x < bbox[1][0]; x++) {
+            pos[0] = x;
+            pos[1] = y;
+            pos[2] = z;
+            mesh_get_at(mesh, &iter, pos, v);
             fwrite(v, 4, 1, file);
         }
         i++;
     }
     fclose(file);
-    mesh_delete(m);
 }
 
 static void export_as_qubicle(const char *path)
@@ -175,7 +170,7 @@ static void export_as_qubicle(const char *path)
     path = path ?: noc_file_dialog_open(NOC_FILE_DIALOG_SAVE,
                     "qubicle\0*.qb\0", NULL, "untitled.qb");
     if (!path) return;
-    qubicle_export(goxel->layers_mesh, path);
+    qubicle_export(goxel->image, path);
 }
 
 ACTION_REGISTER(import_qubicle,
