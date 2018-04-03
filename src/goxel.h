2a1a7338f47d58d40cc0f7e19083d959fa58dc32
Guillaume Chereau <guillaume@noctua-software.com>
2018-04-03 16:15:25 +0800

    More doc

--- src/goxel.h
+++ src/goxel.h
@@ -1243,16 +1243,47 @@ typedef struct inputs
 
 
 
-// #### Mouse gestures #####################################3
+/* #########################################
+ * Section: Mouse gestures
+ * Detect 2d mouse or touch gestures.
+ *
+ * The way it works is that we have to create one instance of
+ * <gesture_t> per gesture we can recognise, and then use the
+ * <gesture_update> function to update their states and call the callback
+ * functions of active gestures.
+ */
 
+/* XXX: This part of the code is not very clear */
+
+/*
+ * Enum: GESTURE_TYPES
+ * Define the different types of recognised gestures.
+ *
+ * GESTURE_DRAG     - Click and drag the mouse.
+ * GESTURE_CLICK    - Single click.
+ * GESTURE_PINCH    - Two fingers pinch.
+ * GESTURE_HOVER    - Move the mouse without clicking.
+ */
 enum {
-    // Supported type of gestures.
     GESTURE_DRAG        = 1 << 0,
     GESTURE_CLICK       = 1 << 1,
     GESTURE_PINCH       = 1 << 2,
     GESTURE_HOVER       = 1 << 3,
+};
 
-    // Gesture states.
+/*
+ * Enum: GESTURE_STATES
+ * Define the states a gesture can be in.
+ *
+ * GESTURE_POSSIBLE     - The gesture is not recognised yet (default state).
+ * GESTURE_RECOGNISED   - The gesture has been recognised.
+ * GESTURE_BEGIN        - The gesture has begun.
+ * GESTURE_UPDATE       - The gesture is in progress.
+ * GESTURE_END          - The testure has ended.
+ * GESTURE_TRIGGERED    - For click gestures: the gesture has occured.
+ * GESTURE_FAILED       - The gesture has failed.
+ */
+enum {
     GESTURE_POSSIBLE = 0,
     GESTURE_RECOGNISED,
     GESTURE_BEGIN,
@@ -1262,6 +1293,10 @@ enum {
     GESTURE_FAILED,
 };
 
+/*
+ * Type: gesture_t
+ * Structure used to handle a given gesture.
+ */
 typedef struct gesture gesture_t;
 struct gesture
 {
@@ -1276,6 +1311,18 @@ struct gesture
     int     (*callback)(const gesture_t *gest, void *user);
 };
 
+/*
+ * Function: gesture_update
+ * Update the state of a list of gestures, and call the gestures
+ * callbacks as needed.
+ *
+ * Parameters:
+ *   nb         - Number of gestures.
+ *   gestures   - A pointer to an array of <gesture_t> instances.
+ *   inputs     - The inputs structure.
+ *   viewport   - Current viewport rect.
+ *   user       - User data pointer, passed to the callbacks.
+ */
 int gesture_update(int nb, gesture_t *gestures[],
                    const inputs_t *inputs, const float viewport[4],
                    void *user);
