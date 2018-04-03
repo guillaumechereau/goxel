2050561b824d61a91cc2c44f56ddc05f695385f7
Guillaume Chereau <guillaume@noctua-software.com>
2018-04-03 16:01:25 +0800

    Some more doc

--- src/goxel.h
+++ src/goxel.h
@@ -241,12 +241,24 @@ enum {
         (a)[2] == (b)[2] && \
         (a)[3] == (b)[3]; })
 
-// Convertion between radian and degree.
+/* Define: DR2D
+ * Convertion ratio from radian to degree. */
 #define DR2D (180 / M_PI)
+
+/* Define: DR2D
+ * Convertion ratio from degree to radian. */
 #define DD2R (M_PI / 180)
 
+/* Define: KB
+ * 1024 */
 #define KB 1024
+
+/* Define: MB
+ * 1024^2 */
 #define MB (1024 * KB)
+
+/* Define: GB
+ * 1024^3 */
 #define GB (1024 * MB)
 
 /*
@@ -353,12 +365,36 @@ char *read_file(const char *path, int *size);
 void dolog(int level, const char *msg,
            const char *func, const char *file, int line, ...);
 
+/*
+ * Function: img_read
+ * Read an image from a file.
+ */
 uint8_t *img_read(const char *path, int *width, int *height, int *bpp);
+
+/*
+ * Function: img_read_from_mem
+ * Read an image from memory.
+ */
 uint8_t *img_read_from_mem(const char *data, int size,
                            int *w, int *h, int *bpp);
+
+/*
+ * Function: img_write
+ * Write an image to a file.
+ */
 void img_write(const uint8_t *img, int w, int h, int bpp, const char *path);
+
+/*
+ * Function: img_write_to_mem
+ * Write an image to memory.
+ */
 uint8_t *img_write_to_mem(const uint8_t *img, int w, int h, int bpp,
                           int *size);
+
+/*
+ * Function: img_downsample
+ * Downsample an image by half, using interpolation.
+ */
 void img_downsample(const uint8_t *img, int w, int h, int bpp,
                     uint8_t *out);
 /*
