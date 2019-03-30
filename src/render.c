242e1eced4b6e04de7e7619366d107a685c48bdd
Guillaume Chereau <guillaume@noctua-software.com>
2019-03-30 16:14:18 +0800

    Change default material settings
    Lower the specular value.

--- src/render.c
+++ src/render.c
@@ -1035,8 +1035,8 @@ int render_get_default_settings(int i, char **name, render_settings_t *out)
     *out = (render_settings_t) {
         .border_shadow = 0.4,
         .ambient = 0.3,
-        .diffuse = 0.5,
-        .specular = 0.4,
+        .diffuse = 0.6,
+        .specular = 0.1,
         .shininess = 2.0,
         .smoothness = 0.0,
         .effects = EFFECT_BORDERS,
