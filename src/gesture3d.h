880eced87149475d7bf6f18ad5a39949045827e9
Guillaume Chereau <guillaume@noctua-software.com>
2019-04-12 13:16:44 +0800

    Move 3d gesture declarations out of goxel.h
    Cleaning up goxel.h.

---
+++ src/gesture3d.h
@@ -0,0 +1,45 @@
+/* Goxel 3D voxels editor
+ *
+ * copyright (c) 2019 Guillaume Chereau <guillaume@noctua-software.com>
+ *
+ * Goxel is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+
+ * Goxel is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+
+ * You should have received a copy of the GNU General Public License along with
+ * goxel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GESTURE3D_H
+#define GESTURE3D_H
+
+typedef struct gesture3d gesture3d_t;
+
+typedef struct cursor {
+    float  pos[3];
+    float  normal[3];
+    int    snap_mask;
+    int    snaped;
+    int    flags; // Union of CURSOR_* values.
+    float  snap_offset; // XXX: fix this.
+} cursor_t;
+
+// #### 3d gestures
+struct gesture3d
+{
+    int         type;
+    int         state;
+    int         buttons; // CURSOR_SHIFT | CURSOR_CTRL
+    cursor_t    *cursor;
+    int         (*callback)(gesture3d_t *gest, void *user);
+};
+
+int gesture3d(gesture3d_t *gest, cursor_t *curs, void *user);
+
+#endif // GESTURE3D_H
--- src/goxel.h
+++ src/goxel.h
@@ -37,6 +37,7 @@
 #include "noc_file_dialog.h"
 #include "block_def.h"
 #include "gesture.h"
+#include "gesture3d.h"
 #include "gui.h"
 #include "inputs.h"
 #include "log.h"
@@ -957,29 +958,6 @@ enum {
 
     CURSOR_OUT          = 1 << 3, // Outside of sensing area.
 };
-
-typedef struct gesture3d gesture3d_t;
-
-typedef struct cursor {
-    float  pos[3];
-    float  normal[3];
-    int    snap_mask;
-    int    snaped;
-    int    flags; // Union of CURSOR_* values.
-    float  snap_offset; // XXX: fix this.
-} cursor_t;
-
-// #### 3d gestures
-struct gesture3d
-{
-    int         type;
-    int         state;
-    int         buttons; // CURSOR_SHIFT | CURSOR_CTRL
-    cursor_t    *cursor;
-    int         (*callback)(gesture3d_t *gest, void *user);
-};
-
-int gesture3d(gesture3d_t *gest, cursor_t *curs, void *user);
 
 enum {
     // Tools flags.
