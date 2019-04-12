142410848ca24e41188fec96d1c51a06de40e379
Guillaume Chereau <guillaume@noctua-software.com>
2019-04-12 11:37:47 +0800

    Add log.h file
    Still cleaning up goxel.h

--- src/goxel.h
+++ src/goxel.h
@@ -36,6 +36,7 @@
 #include "utarray.h"
 #include "noc_file_dialog.h"
 #include "block_def.h"
+#include "log.h"
 #include "luagoxel.h"
 #include "mesh.h"
 #include "texture.h"
@@ -81,35 +82,6 @@
 #define DEFINED___(_, v, ...) v
 // #############################
 
-
-// #### Logging macros #########
-
-enum {
-    GOX_LOG_VERBOSE = 2,
-    GOX_LOG_DEBUG   = 3,
-    GOX_LOG_INFO    = 4,
-    GOX_LOG_WARN    = 5,
-    GOX_LOG_ERROR   = 6,
-};
-
-#ifndef LOG_LEVEL
-#   if DEBUG
-#       define LOG_LEVEL GOX_LOG_DEBUG
-#   else
-#       define LOG_LEVEL GOX_LOG_INFO
-#   endif
-#endif
-
-#define LOG(level, msg, ...) do { \
-    if (level >= LOG_LEVEL) \
-        dolog(level, msg, __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
-} while(0)
-
-#define LOG_V(msg, ...) LOG(GOX_LOG_VERBOSE, msg, ##__VA_ARGS__)
-#define LOG_D(msg, ...) LOG(GOX_LOG_DEBUG,   msg, ##__VA_ARGS__)
-#define LOG_I(msg, ...) LOG(GOX_LOG_INFO,    msg, ##__VA_ARGS__)
-#define LOG_W(msg, ...) LOG(GOX_LOG_WARN,    msg, ##__VA_ARGS__)
-#define LOG_E(msg, ...) LOG(GOX_LOG_ERROR,   msg, ##__VA_ARGS__)
 
 // CHECK is similar to an assert, but the condition is tested even in release
 // mode.
---
+++ src/log.h
@@ -0,0 +1,51 @@
+/* Goxel 3D voxels editor
+ *
+ * copyright (c) 2019 Guillaume Chereau <guillaume@noctua-software.com>
+ *
+ * Goxel is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+
+ * Goxel is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+
+ * You should have received a copy of the GNU General Public License along with
+ * goxel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LOG_H
+#define LOG_H
+
+// #### Logging macros #########
+
+enum {
+    GOX_LOG_VERBOSE = 2,
+    GOX_LOG_DEBUG   = 3,
+    GOX_LOG_INFO    = 4,
+    GOX_LOG_WARN    = 5,
+    GOX_LOG_ERROR   = 6,
+};
+
+#ifndef LOG_LEVEL
+#   if DEBUG
+#       define LOG_LEVEL GOX_LOG_DEBUG
+#   else
+#       define LOG_LEVEL GOX_LOG_INFO
+#   endif
+#endif
+
+#define LOG(level, msg, ...) do { \
+    if (level >= LOG_LEVEL) \
+        dolog(level, msg, __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
+} while(0)
+
+#define LOG_V(msg, ...) LOG(GOX_LOG_VERBOSE, msg, ##__VA_ARGS__)
+#define LOG_D(msg, ...) LOG(GOX_LOG_DEBUG,   msg, ##__VA_ARGS__)
+#define LOG_I(msg, ...) LOG(GOX_LOG_INFO,    msg, ##__VA_ARGS__)
+#define LOG_W(msg, ...) LOG(GOX_LOG_WARN,    msg, ##__VA_ARGS__)
+#define LOG_E(msg, ...) LOG(GOX_LOG_ERROR,   msg, ##__VA_ARGS__)
+
+#endif // LOG_H
