5a71824cd6eff1f93eab183a6900eda4c8e2a645
Guillaume Chereau <guillaume@noctua-software.com>
2019-04-16 16:07:05 +0800

    Minor fix

--- src/utils/sound.h
+++ src/utils/sound.h
@@ -19,7 +19,8 @@
 #include <stdbool.h>
 
 void sound_init(void);
-void sound_play(const char *sound, float volume, float pitch);
+void sound_register(const char *name, const char *wav_data);
+void sound_play(const char *name, float volume, float pitch);
 void sound_iter(void);
 
 bool sound_is_enabled(void);
