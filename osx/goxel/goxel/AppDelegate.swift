5cec3aac12b8a2ebfe13a6cce0a5fb16acd377fc
Guillaume Chereau <guillaume@noctua-software.com>
2019-04-25 18:18:32 +0800

    Attempt to make shortcuts work on OSX
    I map the command key to goxel control.
    
    Also had to use some hack so that the keyup event properly get send.

--- osx/goxel/goxel/AppDelegate.swift
+++ osx/goxel/goxel/AppDelegate.swift
@@ -11,6 +11,20 @@ import OpenGL
 import GLKit
 import AppKit
 import Foundation
+import Carbon.HIToolbox
+
+// Set as the main application in the plist.
+// Used so that we get key up event event with the command key pressed.
+@objc(GoxNSApplication)
+class GoxNSApplication: NSApplication {
+    override func sendEvent(_ event: NSEvent) {
+        if (event.type == .keyUp && event.modifierFlags.contains(.command)) {
+            self.keyWindow?.sendEvent(event)
+        } else {
+            super.sendEvent(event)
+        }
+    }
+}
 
 class GoxNSOpenGLView: NSOpenGLView {
     override func awakeFromNib()
@@ -78,7 +92,7 @@ class GoxNSOpenGLView: NSOpenGLView {
     override func scrollWheel(with event: NSEvent) {
         appDelegate().inputs.mouse_wheel = Float(event.scrollingDeltaY)
     }
-    
+
     override func keyDown(with event: NSEvent) {
         switch (event.keyCode) {
             case 36: appDelegate().inputs.keys.257 = true
@@ -88,6 +102,32 @@ class GoxNSOpenGLView: NSOpenGLView {
             case 124: appDelegate().inputs.keys.262 = true
             case 125: appDelegate().inputs.keys.264 = true
             case 126: appDelegate().inputs.keys.265 = true
+            case UInt16(kVK_ANSI_A): appDelegate().inputs.keys.65 = true
+            case UInt16(kVK_ANSI_B): appDelegate().inputs.keys.66 = true
+            case UInt16(kVK_ANSI_C): appDelegate().inputs.keys.67 = true
+            case UInt16(kVK_ANSI_D): appDelegate().inputs.keys.68 = true
+            case UInt16(kVK_ANSI_E): appDelegate().inputs.keys.69 = true
+            case UInt16(kVK_ANSI_F): appDelegate().inputs.keys.70 = true
+            case UInt16(kVK_ANSI_G): appDelegate().inputs.keys.72 = true
+            case UInt16(kVK_ANSI_H): appDelegate().inputs.keys.72 = true
+            case UInt16(kVK_ANSI_I): appDelegate().inputs.keys.73 = true
+            case UInt16(kVK_ANSI_J): appDelegate().inputs.keys.74 = true
+            case UInt16(kVK_ANSI_K): appDelegate().inputs.keys.75 = true
+            case UInt16(kVK_ANSI_L): appDelegate().inputs.keys.76 = true
+            case UInt16(kVK_ANSI_M): appDelegate().inputs.keys.77 = true
+            case UInt16(kVK_ANSI_N): appDelegate().inputs.keys.78 = true
+            case UInt16(kVK_ANSI_O): appDelegate().inputs.keys.79 = true
+            case UInt16(kVK_ANSI_P): appDelegate().inputs.keys.80 = true
+            case UInt16(kVK_ANSI_Q): appDelegate().inputs.keys.81 = true
+            case UInt16(kVK_ANSI_R): appDelegate().inputs.keys.82 = true
+            case UInt16(kVK_ANSI_S): appDelegate().inputs.keys.83 = true
+            case UInt16(kVK_ANSI_T): appDelegate().inputs.keys.84 = true
+            case UInt16(kVK_ANSI_U): appDelegate().inputs.keys.85 = true
+            case UInt16(kVK_ANSI_V): appDelegate().inputs.keys.86 = true
+            case UInt16(kVK_ANSI_W): appDelegate().inputs.keys.87 = true
+            case UInt16(kVK_ANSI_X): appDelegate().inputs.keys.88 = true
+            case UInt16(kVK_ANSI_Y): appDelegate().inputs.keys.89 = true
+            case UInt16(kVK_ANSI_Z): appDelegate().inputs.keys.90 = true
             default: break
         }
         let scalars = event.characters!.unicodeScalars
@@ -106,13 +146,39 @@ class GoxNSOpenGLView: NSOpenGLView {
             case 124: appDelegate().inputs.keys.262 = false
             case 125: appDelegate().inputs.keys.264 = false
             case 126: appDelegate().inputs.keys.265 = false
+            case UInt16(kVK_ANSI_A): appDelegate().inputs.keys.65 = false
+            case UInt16(kVK_ANSI_B): appDelegate().inputs.keys.66 = false
+            case UInt16(kVK_ANSI_C): appDelegate().inputs.keys.67 = false
+            case UInt16(kVK_ANSI_D): appDelegate().inputs.keys.68 = false
+            case UInt16(kVK_ANSI_E): appDelegate().inputs.keys.69 = false
+            case UInt16(kVK_ANSI_F): appDelegate().inputs.keys.70 = false
+            case UInt16(kVK_ANSI_G): appDelegate().inputs.keys.72 = false
+            case UInt16(kVK_ANSI_H): appDelegate().inputs.keys.72 = false
+            case UInt16(kVK_ANSI_I): appDelegate().inputs.keys.73 = false
+            case UInt16(kVK_ANSI_J): appDelegate().inputs.keys.74 = false
+            case UInt16(kVK_ANSI_K): appDelegate().inputs.keys.75 = false
+            case UInt16(kVK_ANSI_L): appDelegate().inputs.keys.76 = false
+            case UInt16(kVK_ANSI_M): appDelegate().inputs.keys.77 = false
+            case UInt16(kVK_ANSI_N): appDelegate().inputs.keys.78 = false
+            case UInt16(kVK_ANSI_O): appDelegate().inputs.keys.79 = false
+            case UInt16(kVK_ANSI_P): appDelegate().inputs.keys.80 = false
+            case UInt16(kVK_ANSI_Q): appDelegate().inputs.keys.81 = false
+            case UInt16(kVK_ANSI_R): appDelegate().inputs.keys.82 = false
+            case UInt16(kVK_ANSI_S): appDelegate().inputs.keys.83 = false
+            case UInt16(kVK_ANSI_T): appDelegate().inputs.keys.84 = false
+            case UInt16(kVK_ANSI_U): appDelegate().inputs.keys.85 = false
+            case UInt16(kVK_ANSI_V): appDelegate().inputs.keys.86 = false
+            case UInt16(kVK_ANSI_W): appDelegate().inputs.keys.87 = false
+            case UInt16(kVK_ANSI_X): appDelegate().inputs.keys.88 = false
+            case UInt16(kVK_ANSI_Y): appDelegate().inputs.keys.89 = false
+            case UInt16(kVK_ANSI_Z): appDelegate().inputs.keys.90 = false
             default: break
         }
     }
     
     override func flagsChanged(with event: NSEvent) {
         appDelegate().inputs.keys.340 = event.modifierFlags.contains(.shift)
-        appDelegate().inputs.keys.341 = event.modifierFlags.contains(.control)
+        appDelegate().inputs.keys.341 = event.modifierFlags.contains(.command)
     }
 }
 
