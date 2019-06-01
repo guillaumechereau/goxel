402fc02b6589c1c914ebe3f026d94889476b4920
Guillaume Chereau <guillaume@noctua-software.com>
2019-06-01 16:15:04 +0800

    Add static vector size checks in vec.h
    Could prevent some bugs at compile time.

--- src/utils/vec.h
+++ src/utils/vec.h
@@ -59,7 +59,15 @@ static const float mat4_zero[4][4] = {{0, 0, 0, 0},
 static const float quat_identity[4] = {1, 0, 0, 0};
 
 
-DECL void vec2_set(float v[2], float x, float y)
+// S macro for C99 static argument array size.
+#ifndef __cplusplus
+#define S static
+#else
+#define S
+#endif
+
+
+DECL void vec2_set(float v[S 2], float x, float y)
 {
     v[0] = x;
     v[1] = y;
@@ -80,13 +88,13 @@ DECL void vec2_set(float v[2], float x, float y)
 #define vec4_set(v, x, y, z, w) do { \
     (v)[0] = (x); (v)[1] = (y); (v)[2] = (z); (v)[3] = (w); } while(0)
 
-DECL void vec2_copy(const float a[2], float out[2])
+DECL void vec2_copy(const float a[S 2], float out[S 2])
 {
     out[0] = a[0];
     out[1] = a[1];
 }
 
-DECL void vec3_copy(const float a[3], float out[3])
+DECL void vec3_copy(const float a[S 3], float out[S 3])
 {
     out[0] = a[0];
     out[1] = a[1];
@@ -101,12 +109,12 @@ DECL void vec3_copy(const float a[3], float out[3])
         (b)[0] = (a)[0]; (b)[1] = (a)[1]; (b)[2] = (a)[2]; (b)[3] = (a)[3]; \
     } while (0)
 
-DECL bool vec2_equal(const float a[2], const float b[2])
+DECL bool vec2_equal(const float a[S 2], const float b[S 2])
 {
     return a[0] == b[0] && a[1] == b[1];
 }
 
-DECL bool vec3_equal(const float a[3], const float b[3])
+DECL bool vec3_equal(const float a[S 3], const float b[S 3])
 {
     return a[0] == b[0] && a[1] == b[1] && a[2] == b[2];
 }
@@ -121,162 +129,166 @@ DECL bool vec3_equal(const float a[3], const float b[3])
         (a)[2] == (b)[2] && \
         (a)[3] == (b)[3]; })
 
-DECL void vec2_add(const float a[2], const float b[2], float out[2])
+DECL void vec2_add(const float a[S 2], const float b[S 2], float out[S 2])
 {
     out[0] = a[0] + b[0];
     out[1] = a[1] + b[1];
 }
 
-DECL void vec3_add(const float a[3], const float b[3], float out[3])
+DECL void vec3_add(const float a[S 3], const float b[S 3], float out[S 3])
 {
     out[0] = a[0] + b[0];
     out[1] = a[1] + b[1];
     out[2] = a[2] + b[2];
 }
 
-DECL void vec3_iadd(float a[3], const float b[3])
+DECL void vec3_iadd(float a[S 3], const float b[S 3])
 {
     vec3_add(a, b, a);
 }
 
-DECL void vec3_addk(const float a[3], const float b[3], float k, float out[3])
+DECL void vec3_addk(const float a[S 3], const float b[S 3], float k,
+                    float out[S 3])
 {
     out[0] = a[0] + b[0] * k;
     out[1] = a[1] + b[1] * k;
     out[2] = a[2] + b[2] * k;
 }
 
-DECL void vec3_iaddk(float a[3], const float b[3], float k)
+DECL void vec3_iaddk(float a[S 3], const float b[S 3], float k)
 {
     vec3_addk(a, b, k, a);
 }
 
 
-DECL void vec2_sub(const float a[2], const float b[2], float out[2])
+DECL void vec2_sub(const float a[S 2], const float b[S 2], float out[S 2])
 {
     out[0] = a[0] - b[0];
     out[1] = a[1] - b[1];
 }
 
-DECL void vec3_sub(const float a[3], const float b[3], float out[3])
+DECL void vec3_sub(const float a[S 3], const float b[S 3], float out[S 3])
 {
     out[0] = a[0] - b[0];
     out[1] = a[1] - b[1];
     out[2] = a[2] - b[2];
 }
 
-DECL void vec3_isub(float a[3], const float b[3])
+DECL void vec3_isub(float a[S 3], const float b[S 3])
 {
     vec3_sub(a, b, a);
 }
 
-DECL void vec2_mul(const float a[2], float k, float out[2])
+DECL void vec2_mul(const float a[S 2], float k, float out[S 2])
 {
     out[0] = a[0] * k;
     out[1] = a[1] * k;
 }
 
-DECL void vec3_mul(const float a[3], float k, float out[3])
+DECL void vec3_mul(const float a[S 3], float k, float out[S 3])
 {
     out[0] = a[0] * k;
     out[1] = a[1] * k;
     out[2] = a[2] * k;
 }
 
-DECL void vec3_imul(float a[3], float k)
+DECL void vec3_imul(float a[S 3], float k)
 {
     vec3_mul(a, k, a);
 }
 
-DECL void vec3_neg(const float a[3], float out[3])
+DECL void vec3_neg(const float a[S 3], float out[S 3])
 {
     vec3_mul(a, -1, out);
 }
 
-DECL float vec2_dot(const float a[2], const float b[2])
+DECL float vec2_dot(const float a[S 2], const float b[S 2])
 {
     return a[0] * b[0] + a[1] * b[1];
 }
 
-DECL float vec3_dot(const float a[3], const float b[3])
+DECL float vec3_dot(const float a[S 3], const float b[S 3])
 {
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 }
 
-DECL float vec2_norm2(const float a[2])
+DECL float vec2_norm2(const float a[S 2])
 {
     return vec2_dot(a, a);
 }
 
-DECL float vec3_norm2(const float a[3])
+DECL float vec3_norm2(const float a[S 3])
 {
     return vec3_dot(a, a);
 }
 
-DECL float vec2_norm(const float a[2])
+DECL float vec2_norm(const float a[S 2])
 {
     return sqrt(vec2_norm2(a));
 }
 
-DECL void vec2_normalize(const float a[2], float out[2])
+DECL void vec2_normalize(const float a[S 2], float out[S 2])
 {
     vec2_mul(a, 1 / vec2_norm(a), out);
 }
 
-DECL float vec3_norm(const float a[3])
+DECL float vec3_norm(const float a[S 3])
 {
     return sqrt(vec3_norm2(a));
 }
 
-DECL void vec3_normalize(const float a[3], float out[3])
+DECL void vec3_normalize(const float a[S 3], float out[S 3])
 {
     vec3_mul(a, 1 / vec3_norm(a), out);
 }
 
-DECL float vec2_dist2(const float a[2], const float b[2])
+DECL float vec2_dist2(const float a[S 2], const float b[S 2])
 {
     float u[2];
     vec2_sub(a, b, u);
     return vec2_norm2(u);
 }
 
-DECL float vec2_dist(const float a[2], const float b[2])
+DECL float vec2_dist(const float a[S 2], const float b[S 2])
 {
     return sqrt(vec2_dist2(a, b));
 }
 
-DECL float vec3_dist2(const float a[3], const float b[3])
+DECL float vec3_dist2(const float a[S 3], const float b[S 3])
 {
     float d[3];
     vec3_sub(a, b, d);
     return vec3_norm2(d);
 }
 
-DECL float vec3_dist(const float a[3], const float b[3])
+DECL float vec3_dist(const float a[S 3], const float b[S 3])
 {
     return sqrt(vec3_dist2(a, b));
 }
 
-DECL void vec2_mix(const float a[2], const float b[2], float t, float out[2])
+DECL void vec2_mix(const float a[S 2], const float b[S 2], float t,
+                   float out[S 2])
 {
     out[0] = a[0] * (1 - t) + b[0] * t;
     out[1] = a[1] * (1 - t) + b[1] * t;
 }
 
-DECL void vec3_mix(const float a[3], const float b[3], float t, float out[3])
+DECL void vec3_mix(const float a[S 3], const float b[S 3], float t,
+                   float out[S 3])
 {
     out[0] = a[0] * (1 - t) + b[0] * t;
     out[1] = a[1] * (1 - t) + b[1] * t;
     out[2] = a[2] * (1 - t) + b[2] * t;
 }
 
-DECL void vec3_lerp(const float a[3], const float b[3], float t, float out[3])
+DECL void vec3_lerp(const float a[S 3], const float b[S 3], float t,
+                    float out[S 3])
 {
     vec3_mix(a, b, t, out);
 }
 
-DECL bool vec3_lerp_const(const float a[3], const float b[3], float d,
-                          float out[3])
+DECL bool vec3_lerp_const(const float a[S 3], const float b[S 3], float d,
+                          float out[S 3])
 {
     float u[3];
     float d2 = vec3_dist2(a, b);
@@ -290,29 +302,29 @@ DECL bool vec3_lerp_const(const float a[3], const float b[3], float d,
     return false;
 }
 
-DECL bool vec3_ilerp_const(float a[3], const float b[3], float d)
+DECL bool vec3_ilerp_const(float a[S 3], const float b[S 3], float d)
 {
     return vec3_lerp_const(a, b, d, a);
 }
 
-DECL void vec3_project(const float a[3], const float b[3], float out[3])
+DECL void vec3_project(const float a[S 3], const float b[S 3], float out[S 3])
 {
     vec3_mul(b, vec3_dot(a, b) / vec3_dot(b, b), out);
 }
 
-DECL float vec2_cross(const float a[2], const float b[2])
+DECL float vec2_cross(const float a[S 2], const float b[S 2])
 {
     return a[0] * b[1] - a[1] * b[0];
 }
 
-DECL void vec3_cross(const float a[3], const float b[3], float out[3])
+DECL void vec3_cross(const float a[S 3], const float b[S 3], float out[S 3])
 {
     out[0] = a[1] * b[2] - a[2] * b[1];
     out[1] = a[2] * b[0] - a[0] * b[2];
     out[2] = a[0] * b[1] - a[1] * b[0];
 }
 
-DECL void mat4_copy(const float m[4][4], float out[4][4])
+DECL void mat4_copy(const float m[S 4][4], float out[S 4][4])
 {
     int i, j;
     for (i = 0; i < 4; i++)
@@ -320,7 +332,8 @@ DECL void mat4_copy(const float m[4][4], float out[4][4])
             out[i][j] = m[i][j];
 }
 
-DECL void mat4_mul_vec4(const float m[4][4], const float v[4], float out[4])
+DECL void mat4_mul_vec4(const float m[S 4][4], const float v[S 4],
+                        float out[S 4])
 {
     float ret[4] = {};
     int i, j;
@@ -332,15 +345,16 @@ DECL void mat4_mul_vec4(const float m[4][4], const float v[4], float out[4])
     vec4_copy(ret, out);
 }
 
-DECL void mat4_mul_vec3(const float m[4][4], const float v[3], float out[3])
+DECL void mat4_mul_vec3(const float m[S 4][4], const float v[S 3],
+                        float out[S 3])
 {
     float v4[4] = {v[0], v[1], v[2], 1.0f};
     mat4_mul_vec4(m, v4, v4);
     vec3_copy(v4, out);
 }
 
-DECL void mat4_translate(const float m[4][4], float x, float y, float z,
-                         float out[4][4])
+DECL void mat4_translate(const float m[S 4][4], float x, float y, float z,
+                         float out[S 4][4])
 {
     float tmp[4][4];
     int i;
@@ -353,13 +367,13 @@ DECL void mat4_translate(const float m[4][4], float x, float y, float z,
     mat4_copy(tmp, out);
 }
 
-DECL void mat4_itranslate(float m[4][4], float x, float y, float z)
+DECL void mat4_itranslate(float m[S 4][4], float x, float y, float z)
 {
     mat4_translate(m, x, y, z, m);
 }
 
-DECL void mat4_scale(const float m[4][4], float x, float y, float z,
-                     float out[4][4])
+DECL void mat4_scale(const float m[S 4][4], float x, float y, float z,
+                     float out[S 4][4])
 {
     int i;
     float tmp[4][4];
@@ -372,12 +386,12 @@ DECL void mat4_scale(const float m[4][4], float x, float y, float z,
     mat4_copy(tmp, out);
 }
 
-DECL void mat4_iscale(float m[4][4], float x, float y, float z)
+DECL void mat4_iscale(float m[S 4][4], float x, float y, float z)
 {
     mat4_scale(m, x, y, z, m);
 }
 
-DECL bool mat4_invert(const float mat[4][4], float out[4][4])
+DECL bool mat4_invert(const float mat[S 4][4], float out[S 4][4])
 {
     float det;
     int i;
@@ -431,7 +445,7 @@ DECL bool mat4_invert(const float mat[4][4], float out[4][4])
     return true;
 }
 
-DECL void mat4_igrow(float m[4][4], float x, float y, float z)
+DECL void mat4_igrow(float m[S 4][4], float x, float y, float z)
 {
     // XXX: need to optimize this.
     float s[3];
@@ -447,7 +461,8 @@ DECL void mat4_igrow(float m[4][4], float x, float y, float z)
     mat4_iscale(m, s[0], s[1], s[2]);
 }
 
-DECL void mat4_mul(const float a[4][4], const float b[4][4], float out[4][4])
+DECL void mat4_mul(const float a[S 4][4], const float b[S 4][4],
+                   float out[S 4][4])
 {
     int i, j, k;
     float ret[4][4] = {};
@@ -462,12 +477,12 @@ DECL void mat4_mul(const float a[4][4], const float b[4][4], float out[4][4])
     mat4_copy(ret, out);
 }
 
-DECL void mat4_imul(float a[4][4], const float b[4][4])
+DECL void mat4_imul(float a[S 4][4], const float b[S 4][4])
 {
     mat4_mul(a, b, a);
 }
 
-DECL void mat4_ortho(float m[4][4], float left, float right, float bottom,
+DECL void mat4_ortho(float m[S 4][4], float left, float right, float bottom,
                      float top, float nearval, float farval)
 {
     float tx = -(right + left) / (right - left);
@@ -482,7 +497,7 @@ DECL void mat4_ortho(float m[4][4], float left, float right, float bottom,
     mat4_copy(ret, m);
 }
 
-DECL void mat4_perspective(float m[4][4], float fovy, float aspect,
+DECL void mat4_perspective(float m[S 4][4], float fovy, float aspect,
                            float nearval, float farval)
 {
     float radian = fovy * M_PI / 180.f;
@@ -496,7 +511,7 @@ DECL void mat4_perspective(float m[4][4], float fovy, float aspect,
     mat4_copy(ret, m);
 }
 
-DECL void mat4_transpose(const float m[4][4], float out[4][4])
+DECL void mat4_transpose(const float m[S 4][4], float out[S 4][4])
 {
     int i, j;
     float ret[4][4] = {};
@@ -506,15 +521,15 @@ DECL void mat4_transpose(const float m[4][4], float out[4][4])
     mat4_copy(ret, out);
 }
 
-DECL void mat4_set_identity(float m[4][4])
+DECL void mat4_set_identity(float m[S 4][4])
 {
     mat4_copy(mat4_identity, m);
 }
 
 // Similar to gluLookAt
-DECL void mat4_lookat(float m[4][4],
-                      const float eye[3], const float center[3],
-                      const float up[3])
+DECL void mat4_lookat(float m[S 4][4],
+                      const float eye[S 3], const float center[S 3],
+                      const float up[S 3])
 {
     float f[3], s[3], u[3], ret[4][4];
     mat4_set_identity(ret);
@@ -532,7 +547,7 @@ DECL void mat4_lookat(float m[4][4],
     mat4_copy(ret, m);
 }
 
-DECL void quat_set_identity(float q[4])
+DECL void quat_set_identity(float q[S 4])
 {
     q[0] = 1;
     q[1] = 0;
@@ -540,7 +555,7 @@ DECL void quat_set_identity(float q[4])
     q[3] = 0;
 }
 
-DECL void quat_copy(const float q[4], float out[4])
+DECL void quat_copy(const float q[S 4], float out[S 4])
 {
     out[0] = q[0];
     out[1] = q[1];
@@ -548,11 +563,12 @@ DECL void quat_copy(const float q[4], float out[4])
     out[3] = q[3];
 }
 
-DECL void quat_from_axis(float quat[4], float a, float x, float y, float z);
-DECL void quat_to_mat4(const float q[4], float out[4][4]);
+DECL void quat_from_axis(float quat[S 4], float a, float x, float y, float z);
+DECL void quat_to_mat4(const float q[S 4], float out[S 4][4]);
 
-DECL void mat4_rotate(const float m[4][4], float a, float x, float y, float z,
-                      float out[4][4])
+DECL void mat4_rotate(const float m[S 4][4],
+                      float a, float x, float y, float z,
+                      float out[S 4][4])
 {
     if (a == 0.0) {
         mat4_copy(m, out);
@@ -588,12 +604,12 @@ DECL void mat4_rotate(const float m[4][4], float a, float x, float y, float z,
     mat4_mul(m, tmp, out);
 }
 
-DECL void mat4_irotate(float m[4][4], float a, float x, float y, float z)
+DECL void mat4_irotate(float m[S 4][4], float a, float x, float y, float z)
 {
     mat4_rotate(m, a, x, y, z, m);
 }
 
-DECL bool mat4_equal(const float a[4][4], const float b[4][4])
+DECL bool mat4_equal(const float a[S 4][4], const float b[S 4][4])
 {
     int i, j;
     for (i = 0; i < 4; i++)
@@ -602,7 +618,7 @@ DECL bool mat4_equal(const float a[4][4], const float b[4][4])
     return true;
 }
 
-DECL void quat_from_axis(float quat[4], float a, float x, float y, float z)
+DECL void quat_from_axis(float quat[S 4], float a, float x, float y, float z)
 {
     float sin_angle;
     float vn[3] = {x, y, z};
@@ -615,7 +631,7 @@ DECL void quat_from_axis(float quat[4], float a, float x, float y, float z)
     quat[3] = vn[2] * sin_angle;
 }
 
-DECL void quat_to_mat4(const float q[4], float out[4][4])
+DECL void quat_to_mat4(const float q[S 4], float out[S 4][4])
 {
     float w, x, y, z;
     w = q[0];
@@ -630,14 +646,14 @@ DECL void quat_to_mat4(const float q[4], float out[4][4])
     mat4_copy(ret, out);
 }
 
-DECL void quat_conjugate(const float q[4], float out[4]) {
+DECL void quat_conjugate(const float q[S 4], float out[S 4]) {
     out[0] =  q[0];
     out[1] = -q[1];
     out[2] = -q[2];
     out[3] = -q[3];
 }
 
-DECL void quat_mul(const float a[4], const float b[4], float out[4])
+DECL void quat_mul(const float a[S 4], const float b[S 4], float out[S 4])
 {
     out[0] = a[0] * b[0] - a[1] * b[1] - a[2] * b[2] - a[3] * b[3];
     out[1] = a[0] * b[1] + a[1] * b[0] + a[2] * b[3] - a[3] * b[2];
@@ -645,65 +661,67 @@ DECL void quat_mul(const float a[4], const float b[4], float out[4])
     out[3] = a[0] * b[3] + a[3] * b[0] + a[1] * b[2] - a[2] * b[1];
 }
 
-DECL void quat_imul(float a[4], const float b[4])
+DECL void quat_imul(float a[S 4], const float b[S 4])
 {
     quat_mul(a, b, a);
 }
 
-DECL void quat_rotate(const float q[4], float a, float x, float y, float z,
-                      float out[4])
+DECL void quat_rotate(const float q[S 4], float a, float x, float y, float z,
+                      float out[S 4])
 {
     float other[4];
     quat_from_axis(other, a, x, y, z);
     quat_mul(q, other, out);
 }
 
-DECL void quat_irotate(float q[4], float a, float x, float y, float z)
+DECL void quat_irotate(float q[S 4], float a, float x, float y, float z)
 {
     quat_rotate(q, a, x, y, z, q);
 }
 
-DECL void quat_mul_vec4(const float q[4], const float v[4], float out[4])
+DECL void quat_mul_vec4(const float q[S 4], const float v[S 4], float out[S 4])
 {
     float m[4][4];
     quat_to_mat4(q, m);
     mat4_mul_vec4(m, v, out);
 }
 
-DECL void mat4_mul_quat(const float mat[4][4], const float q[4],
-                        float out[4][4])
+DECL void mat4_mul_quat(const float mat[S 4][4], const float q[S 4],
+                        float out[S 4][4])
 {
     float qm[4][4];
     quat_to_mat4(q, qm);
     mat4_mul(mat, qm, out);
 }
 
-DECL void mat4_imul_quat(float mat[4][4], const float q[4])
+DECL void mat4_imul_quat(float mat[S 4][4], const float q[S 4])
 {
     mat4_mul_quat(mat, q, mat);
 }
 
-void mat3_to_eul(const float m[3][3], int order, float e[3]);
-void mat3_to_eul2(const float m[3][3], int order, float e1[3], float e2[3]);
-void quat_to_mat3(const float q[4], float out[3][3]);
+void mat3_to_eul(const float m[S 3][3], int order, float e[S 3]);
+void mat3_to_eul2(const float m[S 3][3], int order, float e1[S 3],
+                  float e2[S 3]);
+void quat_to_mat3(const float q[S 4], float out[S 3][3]);
 
-DECL void quat_to_eul(const float q[4], int order, float e[3])
+DECL void quat_to_eul(const float q[S 4], int order, float e[S 3])
 {
     float m[3][3];
     quat_to_mat3(q, m);
     mat3_to_eul(m, order, e);
 }
 
-DECL void quat_to_eul2(const float q[4], int order, float e1[3], float e2[3])
+DECL void quat_to_eul2(const float q[S 4], int order, float e1[S 3],
+                       float e2[S 3])
 {
     float m[3][3];
     quat_to_mat3(q, m);
     mat3_to_eul2(m, order, e1, e2);
 }
 
-void eul_to_quat(const float e[3], int order, float out[4]);
+void eul_to_quat(const float e[S 3], int order, float out[S 4]);
 
-DECL void quat_normalize(const float q[4], float out[4])
+DECL void quat_normalize(const float q[S 4], float out[S 4])
 {
     float n = sqrt(q[0] * q[0] + q[1] * q[1] + q[2] * q[2] + q[3] * q[3]);
     out[0] = q[0] / n;
@@ -711,5 +729,7 @@ DECL void quat_normalize(const float q[4], float out[4])
     out[2] = q[2] / n;
     out[3] = q[3] / n;
 }
+
+#undef S
 
 #endif // VEC_H_
