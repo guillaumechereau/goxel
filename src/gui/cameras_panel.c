048668fb8e1e15719eb222aa5a2c7af632adda5c
Guillaume Chereau <guillaume@noctua-software.com>
2019-10-07 12:32:24 +0800

    Add camera pitch/yaw inputs

--- src/gui/cameras_panel.c
+++ src/gui/cameras_panel.c
@@ -23,6 +23,8 @@ void gui_cameras_panel(void)
     camera_t *cam;
     int i = 0;
     bool current;
+    float rot[3][3], e1[3], e2[3], eul[3], pitch, yaw, v;
+
     gui_group_begin(NULL);
     DL_FOREACH(goxel.image->cameras, cam) {
         current = goxel.image->active_camera == cam;
@@ -63,6 +65,31 @@ void gui_cameras_panel(void)
     gui_action_button("view_front", "front", 0.5, ""); gui_same_line();
     gui_action_button("view_top", "top", 1.0, "");
     gui_action_button("view_default", "default", 1.0, "");
+    gui_group_end();
+
+    // Allow to edit euler angles (Should this be a generic widget?)
+    gui_group_begin(NULL);
+    mat4_to_mat3(cam->mat, rot);
+    mat3_to_eul2(rot, EULER_ORDER_XYZ, e1, e2);
+    if (fabs(e1[1]) < fabs(e2[1]))
+        vec3_copy(e1, eul);
+    else
+        vec3_copy(e2, eul);
+
+    pitch = round(eul[0] * DR2D);
+    if (pitch < 0) pitch += 360;
+    v = pitch;
+    if (gui_input_float("Pitch", &v, 1, -90, 90, "%.0f")) {
+        v = (v - pitch) * DD2R;
+        camera_turntable(cam, 0, v);
+    }
+
+    yaw = round(eul[2] * DR2D);
+    v = yaw;
+    if (gui_input_float("Yaw", &v, 1, -180, 180, "%.0f")) {
+        v = (v - yaw) * DD2R;
+        camera_turntable(cam, v, 0);
+    }
     gui_group_end();
 }
 
