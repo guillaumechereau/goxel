2ae26a4466cd83577c70aed0a7a52a5b57b9ceae
Guillaume Chereau <guillaume@noctua-software.com>
2020-08-23 22:11:28 +0800

    Keep working on quickj intergration
    Still very experimental at the moment.

--- src/script.c
+++ src/script.c
@@ -19,32 +19,95 @@
 #include "goxel.h"
 #include "../ext_src/quickjs/quickjs.h"
 
-typedef struct klass klass_t;
+enum {
+    ATTR_STR_BUF =      1 << 0,
+};
 
-typedef struct {
+typedef struct klass klass_t;
+typedef struct attribute attribute_t;
+
+struct attribute {
     const char *name;
-    int type;
     klass_t *klass;
+    int flags;
+
     struct {
         int offset;
         int size;
     } member;
-} attribute_t;
+
+    struct {
+        const char *type;
+    } list;
+
+    JSValue (*getter)(JSContext *ctx, JSValueConst this_val);
+    JSCFunction *fn;
+};
 
 struct klass {
     JSClassID id;
     JSClassDef def;
+    JSValue (*create)(JSContext *ctx, void *parent, const attribute_t *attr);
     attribute_t attributes[];
 };
+
+typedef struct list_el list_el_t;
+struct list_el {
+    list_el_t *next, *prev;
+};
+
+typedef struct {
+    list_el_t **ptr;
+    const char *type;
+} list_t;
 
 #define MEMBER(k, m) .member = {offsetof(k, m), sizeof(((k*)0)->m)}
 
 static klass_t image_klass;
+static klass_t list_klass;
+
+static JSValue new_obj_ref(JSContext *ctx, void *obj, const klass_t *klass)
+{
+    JSValue ret;
+    ret = JS_NewObjectClass(ctx, klass->id);
+    JS_SetOpaque(ret, obj);
+    return ret;
+}
+
+static JSValue list_create(JSContext *ctx, void *parent,
+                           const attribute_t *attr)
+{
+    list_t *list;
+    list = calloc(1, sizeof(*list));
+    list->ptr = parent + attr->member.offset;
+    return new_obj_ref(ctx, list, &list_klass);
+}
+
+static JSValue list_add_new(JSContext *ctx, JSValueConst this_val,
+                            int argc, JSValueConst *argv)
+{
+    list_t *list;
+    list = JS_GetOpaque(this_val, list_klass.id);
+
+    return JS_NewInt32(ctx, 10);
+}
+
+static JSValue list_length_get(JSContext *ctx, JSValueConst this_val)
+{
+    int length;
+    list_t *list;
+    list_el_t *el;
+    list = JS_GetOpaque(this_val, list_klass.id);
+    DL_COUNT(*list->ptr, el, length);
+    return JS_NewInt32(ctx, length);
+}
 
 static klass_t list_klass = {
     .def = { "List" },
+    .create = list_create,
     .attributes = {
-        {}
+        {"length", .getter=list_length_get},
+        {"addNew", .fn=list_add_new}
     },
 };
 
@@ -59,23 +122,24 @@ static klass_t goxel_klass = {
 static klass_t image_klass = {
     .def = { "Image" },
     .attributes = {
-        {"layers", .klass=&list_klass, MEMBER(image_t, layers)},
+        {"layers", .klass=&list_klass, .list={.type="Layer"},
+         MEMBER(image_t, layers)},
         {}
     },
 };
 
-static JSValue new_obj_ref(JSContext *ctx, void *obj, const klass_t *klass)
-{
-    JSValue ret;
-    ret = JS_NewObjectClass(ctx, klass->id);
-    JS_SetOpaque(ret, obj);
-    return ret;
-}
+static klass_t layer_klass = {
+    .def = { "Layer" },
+    .attributes = {
+        {"name", .flags=ATTR_STR_BUF, MEMBER(layer_t, name)},
+        {}
+    },
+};
 
 static JSValue obj_attr_getter(JSContext *ctx, JSValueConst this_val,
                                int magic)
 {
-    void *obj, *ptr;
+    void *obj, **ptr;
     const klass_t *klass;
     const attribute_t *attr;
     JSValue proto;
@@ -86,9 +150,12 @@ static JSValue obj_attr_getter(JSContext *ctx, JSValueConst this_val,
     JS_FreeValue(ctx, proto);
 
     attr = &klass->attributes[magic];
+    if (attr->klass->create) {
+        return attr->klass->create(ctx, obj, attr);
+    }
     if (attr->klass && attr->member.size) {
         ptr = obj + attr->member.offset;
-        return new_obj_ref(ctx, ptr, attr->klass);
+        return new_obj_ref(ctx, *ptr, attr->klass);
     }
 
     return JS_NewInt32(ctx, magic);
@@ -99,6 +166,7 @@ static void init_klass(JSContext *ctx, klass_t *klass)
     JSValue proto, getter;
     attribute_t *attr;
     int i;
+    JSAtom name;
 
     JS_NewClassID(&klass->id);
     JS_NewClass(JS_GetRuntime(ctx), klass->id, &klass->def);
@@ -107,10 +175,20 @@ static void init_klass(JSContext *ctx, klass_t *klass)
     JS_SetClassProto(ctx, klass->id, proto);
 
     for (i = 0, attr = &klass->attributes[0]; attr->name; attr++, i++) {
-        getter = JS_NewCFunction2(ctx, (void*)obj_attr_getter, NULL, 0,
-                                  JS_CFUNC_getter_magic, 0);
-        JS_DefinePropertyGetSet(ctx, proto, JS_NewAtom(ctx, attr->name),
-                                getter, JS_UNDEFINED, i);
+        name = JS_NewAtom(ctx, attr->name);
+        if (attr->getter) {
+            getter = JS_NewCFunction2(ctx, (void*)attr->getter, NULL, 0,
+                                      JS_CFUNC_getter, 0);
+            JS_DefinePropertyGetSet(ctx, proto, name, getter, JS_UNDEFINED, 0);
+        } else if (attr->fn) {
+            JS_DefinePropertyValue(ctx, proto, name,
+                           JS_NewCFunction(ctx, attr->fn, NULL, 0),
+                           JS_DEF_CFUNC);
+        } else {
+            getter = JS_NewCFunction2(ctx, (void*)obj_attr_getter, NULL, 0,
+                                      JS_CFUNC_getter_magic, i);
+            JS_DefinePropertyGetSet(ctx, proto, name, getter, JS_UNDEFINED, 0);
+        }
     }
 }
 
@@ -135,19 +213,6 @@ static JSValue js_print(JSContext *ctx, JSValueConst this_val,
     return JS_UNDEFINED;
 }
 
-/*
-static JSValue gox_obj_attr_getter(JSContext *ctx, JSValueConst this_val,
-                                   int argc, JSValueConst *argv, int magic,
-                                   JSValue *func_data)
-{
-    const char *name;
-    name = JS_ToCString(ctx, *func_data);
-    if (strcmp(name, "image")) {
-    }
-    return JS_UNDEFINED;
-}
-*/
-
 static void js_std_add_helpers(JSContext *ctx, int argc, char **argv)
 {
     JSValue global_obj, console, goxel_obj;
@@ -160,6 +225,7 @@ static void js_std_add_helpers(JSContext *ctx, int argc, char **argv)
     init_klass(ctx, &list_klass);
     init_klass(ctx, &goxel_klass);
     init_klass(ctx, &image_klass);
+    init_klass(ctx, &layer_klass);
 
     goxel_obj = new_obj_ref(ctx, &goxel, &goxel_klass);
     JS_SetPropertyStr(ctx, global_obj, "goxel", goxel_obj);
