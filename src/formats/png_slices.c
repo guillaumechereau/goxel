a906c9e9a2c633f155f13d7854998ced165a08b5
FlyOrBoom <31298876+FlyOrBoom@users.noreply.github.com>
2022-01-29 01:16:41 -0800

    Rudimentary GUI for PNG slice order
    Change from permutations to 2 specified directions:
    One for the axis the slices are normal to, and the other being
    the axis along which the slices are laid down flat across.

--- src/formats/png_slices.c
+++ src/formats/png_slices.c
@@ -19,20 +19,29 @@
 #include "goxel.h"
 #include "file_format.h"
 
-static int export_as_png_slices(const image_t *image, const char *path)
+typedef struct {
+    int slicing_direction;
+    int laying_direction;
+} export_options_t;
+
+static export_options_t g_export_options = {};
+
+static void export_gui(void)
+{
+    gui_input_int("Slicing direction", &g_export_options.slicing_direction, 0, 2);
+    gui_input_int("Laying direction", &g_export_options.laying_direction, 0, 2);
+}
+
+static void png_slices_export(const image_t *image,
+                             const export_options_t *options,
+                             const char *path)
 {
     float box[4][4];
     const mesh_t *mesh;
-    int order = 0; // TODO: make this a setting in the export panel
-    int perm[6][3] = {
-        { 0, 1, 2 },
-        { 0, 2, 1 },
-        { 1, 0, 2 },
-        { 1, 2, 0 },
-        { 2, 0, 1 },
-        { 2, 1, 0 }
-    };
-    int i, j, k, x, y, z, w, h, d, pos[3], start_pos[3];
+    int s, m, l,
+        x, y, z,
+        w, h, d,
+        pos[3], start_pos[3];
     uint8_t c[4];
     uint8_t *img;
     mesh_iterator_t iter = {0};
@@ -40,12 +49,16 @@ static int export_as_png_slices(const image_t *image, const char *path)
     mesh = goxel_get_layers_mesh(image);
     mat4_copy(image->box, box);
     if (box_is_null(box)) mesh_get_box(mesh, true, box);
-    i = perm[order][0];
-    j = perm[order][1];
-    k = perm[order][2];
-    w = box[i][i] * 2;
-    h = box[j][j] * 2;
-    d = box[k][k] * 2;
+    
+    s = options->slicing_direction;
+    l = options->laying_direction;
+    if(s == l) l = 2 - s;
+    m = 3 - s - l;
+
+    d = box[s][s] * 2;
+    h = box[m][m] * 2;
+    w = box[l][l] * 2;
+
     start_pos[0] = box[3][0] - box[0][0];
     start_pos[1] = box[3][1] - box[1][1];
     start_pos[2] = box[3][2] - box[2][2];
@@ -53,9 +66,9 @@ static int export_as_png_slices(const image_t *image, const char *path)
     for (z = 0; z < d; z++)
     for (y = 0; y < h; y++)
     for (x = 0; x < w; x++) {
-        pos[i] = x + start_pos[i];
-        pos[j] = y + start_pos[j];
-        pos[k] = z + start_pos[k];
+        pos[l] = x + start_pos[l];
+        pos[m] = y + start_pos[m];
+        pos[s] = z + start_pos[s];
         mesh_get_at(mesh, &iter, pos, c);
         img[(y * w * d + z * w + x) * 4 + 0] = c[0];
         img[(y * w * d + z * w + x) * 4 + 1] = c[1];
@@ -64,11 +77,17 @@ static int export_as_png_slices(const image_t *image, const char *path)
     }
     img_write(img, w * d, h, 4, path);
     free(img);
+}
+
+static int export_as_png_slices(const image_t *img, const char *path)
+{
+    png_slices_export(img, &g_export_options, path);
     return 0;
 }
 
 FILE_FORMAT_REGISTER(png_slices,
     .name = "png slices",
     .ext = "png\0*.png\0",
+    .export_gui = export_gui,
     .export_func = export_as_png_slices,
 )
