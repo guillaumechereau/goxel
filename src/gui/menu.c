625515f203e009b0c8407eb4e694c223f09ecd91
Aditya <75035219+pegvin@users.noreply.github.com>
2022-07-30 19:32:48 +0530

    Add URL for Bug Reports & Contributions

--- src/gui/menu.c
+++ src/gui/menu.c
@@ -5,6 +5,27 @@
 int gui_settings_popup(void *data);
 int gui_about_popup(void *data);
 
+#if defined(_WIN32)
+    #include <windows.h>
+    #include <shellapi.h>
+#endif
+
+void OpenUrlInBrowser(const char* url) {
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__)
+    char command[4096];
+#endif
+
+#if defined(__linux__) || defined(__FreeBSD__)
+    sprintf(command, "xdg-open \"%s\"", url);
+    system(command);
+#elif defined(__APPLE__)
+    sprintf(command, "open \"%s\"", url);
+    system(command);
+#elif defined(_WIN32)
+    ShellExecute(0, 0, url, 0, 0, SW_SHOW);
+#endif
+}
+
 static void import_image_plane(void)
 {
     const char *path;
@@ -70,6 +91,13 @@ void gui_menu(void)
     if (gui_menu_begin("Help")) {
         if (gui_menu_item(0, "About", true))
             gui_open_popup("About", 0, NULL, gui_about_popup);
+
+        if (gui_menu_item(0, "Found a bug?", true))
+            OpenUrlInBrowser("https://github.com/pegvin/goxel2/issues/new/choose");
+
+        if (gui_menu_item(0, "Want to contribute?", true))
+            OpenUrlInBrowser("https://github.com/pegvin/goxel2/blob/master/CONTRIBUTING.md");
+
         gui_menu_end();
     }
 }
