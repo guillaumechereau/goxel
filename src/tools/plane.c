794de9ac11bd7cfa05dd962d3060e104a34936a6
Guillaume Chereau <guillaume@noctua-software.com>
2022-08-15 20:56:28 +0800

    Start to support non 90° angles for plane tool
    Not tested much yet.

--- src/tools/plane.c
+++ src/tools/plane.c
@@ -20,6 +20,7 @@
 
 typedef struct {
     tool_t tool;
+    int move_mode;
 } tool_plane_t;
 
 static int iter(tool_t *tool, const painter_t *painter,
@@ -42,27 +43,63 @@ static int iter(tool_t *tool, const painter_t *painter,
     return 0;
 }
 
-static int gui(tool_t *tool)
+static void mat4_apply_quat(float mat[4][4], const float quat[4])
+{
+    float rot[3][3];
+    int i, j;
+    quat_to_mat3(quat, rot);
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            mat[i][j] = rot[i][j];
+
+}
+
+static int gui(tool_t *tool_)
 {
     int i;
     bool v;
+    float rot[3][3];
+    float quat[4];
 
+    tool_plane_t *tool = (tool_plane_t*)tool_;
     v = goxel.snap_mask & SNAP_PLANE;
     if (gui_checkbox("Visible", &v, NULL)) {
         set_flag(&goxel.snap_mask, SNAP_PLANE, v);
     }
 
-    gui_group_begin(NULL);
-    i = 0;
-    if (gui_input_int("Move", &i, 0, 0))
-        mat4_itranslate(goxel.plane, 0, 0, -i);
-    i = 0;
-    if (gui_input_int("Rot X", &i, 0, 0))
-        mat4_irotate(goxel.plane, i * M_PI / 2, 1, 0, 0);
-    i = 0;
-    if (gui_input_int("Rot Y", &i, 0, 0))
-        mat4_irotate(goxel.plane, i * M_PI / 2, 0, 1, 0);
-    gui_group_end();
+    gui_combo("Move", &tool->move_mode, (const char*[]) {
+              "Relative", "Absolute"}, 2);
+
+    switch (tool->move_mode) {
+    case 0:
+        gui_group_begin(NULL);
+        i = 0;
+        if (gui_input_int("Move", &i, 0, 0))
+            mat4_itranslate(goxel.plane, 0, 0, -i);
+        i = 0;
+        if (gui_input_int("Rot X", &i, 0, 0))
+            mat4_irotate(goxel.plane, i * M_PI / 2, 1, 0, 0);
+        i = 0;
+        if (gui_input_int("Rot Y", &i, 0, 0))
+            mat4_irotate(goxel.plane, i * M_PI / 2, 0, 1, 0);
+        gui_group_end();
+        break;
+
+    case 1:
+        gui_group_begin("Origin");
+        gui_input_float("X", &goxel.plane[3][0], 1.0, 0, 0, NULL);
+        gui_input_float("Y", &goxel.plane[3][1], 1.0, 0, 0, NULL);
+        gui_input_float("Z", &goxel.plane[3][2], 1.0, 0, 0, NULL);
+        gui_group_end();
+
+        mat4_to_mat3(goxel.plane, rot);
+        mat3_to_quat(rot, quat);
+        if (gui_quat("Rotation", quat)) {
+            mat4_apply_quat(goxel.plane, quat);
+        }
+        break;
+
+    }
     return 0;
 }
 
