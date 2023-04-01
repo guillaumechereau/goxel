2b3a542837020a85d92b6d4ce8e959a655ac86ef
Guillaume Chereau <guillaume@noctua-software.com>
2023-04-01 15:38:22 +0800

    Bump version to 0.12.0

--- src/goxel.h
+++ src/goxel.h
@@ -72,7 +72,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define GOXEL_VERSION_STR "0.11.1"
+#define GOXEL_VERSION_STR "0.12.0"
 #ifndef GOXEL_DEFAULT_THEME
 #   define GOXEL_DEFAULT_THEME "original"
 #endif
