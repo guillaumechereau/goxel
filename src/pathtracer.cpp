6d007243299a2f39c4a6e01ab6c584a34a81cd2f
Guillaume Chereau <guillaume@noctua-software.com>
2023-05-14 16:52:04 +0800

    Fix compilation with gcc

--- src/pathtracer.cpp
+++ src/pathtracer.cpp
@@ -315,12 +315,12 @@ static void update_scene(pathtracer_t *pt)
             pos[2] = 0;
         }
         p->scene.shapes.push_back({
+            .quads = {{0, 1, 3, 2}},
             .positions = {
                 {-0.5, -0.5, 0}, {0.5, -0.5, 0}, {-0.5, 0.5, 0}, {0.5, 0.5, 0}
             },
             .normals = {{0, 0, 1}, {0, 0, 1}, {0, 0, 1}, {0, 0, 1}},
             .colors = {color, color, color, color},
-            .quads = {{0, 1, 3, 2}},
         });
         p->scene.instances.push_back({
             .frame = translation_frame({pos[0], pos[1], pos[2]}) *
