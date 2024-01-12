e5809bf87985f84ddfa420829b3114c0d8de518a
Guillaume Chereau <guillaume@noctua-software.com>
2024-01-12 09:10:57 +0800

    Apply rotations at volume center
    Later should probably add the option to manually specify the origin.

--- src/tools/move.c
+++ src/tools/move.c
@@ -27,16 +27,20 @@ typedef struct {
     tool_t tool;
 } tool_move_t;
 
-static void do_move(layer_t *layer, const float mat[4][4])
+static void do_move(layer_t *layer, const float mat[4][4],
+                    const float origin[3])
 {
     float m[4][4] = MAT4_IDENTITY;
+    const float default_origin[3] = {0.5, 0.5, 0.5};
+
+    if (origin == NULL) origin = default_origin;
 
     if (mat4_equal(mat, mat4_identity)) return;
     // Change referential to the volume origin.
     // XXX: maybe this should be done in volume_move directy??
-    mat4_itranslate(m, -0.5, -0.5, -0.5);
+    mat4_itranslate(m, +origin[0], +origin[1], +origin[2]);
     mat4_imul(m, mat);
-    mat4_itranslate(m, +0.5, +0.5, +0.5);
+    mat4_itranslate(m, -origin[0], -origin[1], -origin[2]);
 
     mat4_mul(mat, layer->mat, layer->mat);
     if (layer->base_id || layer->image || layer->shape) {
@@ -44,7 +48,7 @@ static void do_move(layer_t *layer, const float mat[4][4])
     } else {
         volume_move(layer->volume, m);
         if (!box_is_null(layer->box)) {
-            mat4_mul(mat, layer->box, layer->box);
+            mat4_mul(m, layer->box, layer->box);
             box_get_bbox(layer->box, layer->box);
         }
     }
@@ -58,15 +62,31 @@ static int iter(tool_t *tool, const painter_t *painter,
     layer_t *layer = goxel.image->active_layer;
     if (box_edit(SNAP_LAYER_OUT, goxel.tool_drag_mode, transf, &first)) {
         if (first) image_history_push(goxel.image);
-        do_move(layer, transf);
+        do_move(layer, transf, NULL);
     }
     return 0;
+}
+
+static void compute_origin(const layer_t *layer, float origin[3])
+{
+    int bbox[2][3];
+
+    volume_get_bbox(layer->volume, bbox, true);
+    origin[0] = (bbox[0][0] + bbox[1][0] - 1) / 2.0;
+    origin[1] = (bbox[0][1] + bbox[1][1] - 1) / 2.0;
+    origin[2] = (bbox[0][2] + bbox[1][2] - 1) / 2.0;
+
+    // Make sure we always center on a grid point.
+    origin[0] = floor(origin[0]) + 0.5;
+    origin[1] = floor(origin[1]) + 0.5;
+    origin[2] = floor(origin[2]) + 0.5;
 }
 
 static int gui(tool_t *tool)
 {
     layer_t *layer;
     float mat[4][4] = MAT4_IDENTITY, v;
+    float origin[3];
     int i;
     int x, y, z;
 
@@ -137,7 +157,8 @@ static int gui(tool_t *tool)
 
     if (memcmp(&mat, &mat4_identity, sizeof(mat))) {
         image_history_push(goxel.image);
-        do_move(layer, mat);
+        compute_origin(layer, origin);
+        do_move(layer, mat, origin);
     }
     return 0;
 }
